f144bb99b648f5e33b18ee38245f1c55383b1e59
雅樹 森 <marlcola2@gmail.com>
2017-12-18 01:26:47 +0900

    ファイルリストクラス作成

--- CppSampleCode/CppSampleCode.cpp
+++ CppSampleCode/CppSampleCode.cpp
@@ -3,17 +3,38 @@
 #include "stdafx.h"
 #include <Windows.h>
 #include <stdio.h>
+#include <tchar.h>
 
-int main()
-{
+// ファイルリストクラス
+// 
+class FileList {
+public:
+	// ファイルリストを出力する
+	void outputFileList();
 
+	void outputFileList(LPCTSTR basePath);
+};
+
+void FileList::outputFileList() {
+	// 検索ベースパス
+	LPCTSTR basePath = _T("C:\\Data\\Download\\Image\\*.*");
+	
+	FileList fileList;
+
+	fileList.outputFileList(basePath);
+}
+
+void FileList::outputFileList(LPCTSTR basePath) {
+	// 検索ハンドラ
 	HANDLE hFind;
-
+	// データバッファ
 	WIN32_FIND_DATA fd;
 
+	// ベースパスの出力
+	_tprintf(_T("%s\n"), basePath);
 
-	hFind = FindFirstFile(_T("C:\\Data\\Download\\Image\\*.*"), &fd);
-
+	// 検索実行
+	hFind = FindFirstFile(basePath, &fd);
 	do {
 
 		if (fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
@@ -23,10 +44,19 @@ int main()
 			printf("%s\n", fd.cFileName);
 		}
 
-
 	} while (FindNextFile(hFind, &fd));
-
-	return 0;
 }
 
 
+int main()
+{
+	// ファイルリストオブジェクト作成
+	FileList fileList;
+	// ファイルリスト出力
+	fileList.outputFileList();
+	fileList.outputFileList(_T("C:\\*.*"));
+	//入力待ち
+	getchar();
+
+	return 0;
+}
